e05f06a749e1d0e2f03d9ab0c0c31ad6afc9be2e
Steve132 <steve@soapforge.com>
2021-07-27 18:21:42 -0400

    merge

--- test_subprocess_oop.cpp
+++ test_subprocess_oop.cpp
@@ -67,6 +67,7 @@ int main(int argc,char** argv)
 	//const char *const cmd[] = {"/usr/bin/ffmpeg", "-v","24","-pix_fmts", 0};
     const char* cmd[] = {"/usr/bin/ffmpeg", "-i", "test.mp4", "-pix_fmt", "rgba", "-vcodec", "rawvideo", "-f", "image2pipe", "pipe:1", 0};
 
+
 	manual_subprocess_class(cmd);
 
     return 0;
