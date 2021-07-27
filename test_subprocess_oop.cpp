e2940e1cf01a7875d647bd3c2dbf6f25a714b728
sarah <heretodev@gmail.com>
2021-07-27 17:43:03 -0400

    Updated subprocess test case to use new Subprocess.

--- test_subprocess_oop.cpp
+++ test_subprocess_oop.cpp
@@ -33,6 +33,33 @@ int manual_subprocess(const char* const* cmd)
 	return 0;
 }
 
+int manual_subprocess_class(const char* const* cmd)
+{
+    Subprocess sp{cmd};
+
+	int nchars = 128, nerrchars = nchars * 2;
+	char buf[nchars], errbuf[nerrchars];
+	memset(buf, '\0', nchars);
+	memset(errbuf, '\0', nerrchars);
+	int ret2 = sp.read_from_stderr(errbuf, nerrchars);
+	if(ret2 == 0)
+	{
+		std::cout << "subprocess_read_stderr completion of process, no data read." << std::endl;
+		return -1;
+	}
+	std::cout << "err ret:" << ret2 << std::endl;
+	std::cout << "errbuf:" << errbuf << std::endl;
+	int ret = sp.read_from_stdout(buf, nchars); // struct subprocess_s *const process, char *const buffer,unsigned size);
+	if(ret == 0) // TODO: this handling is missing in struct initialization for SPREAD
+	{
+		std::cout << "subprocess_read_stdout completion of process, no data read." << std::endl;
+		return -1;
+	}
+	std::cout << "out ret:" << ret << std::endl;
+	std::cout << "out buf:" << buf << std::endl;
+	return 0;
+}
+
 int main(int argc,char** argv)
 {
     std::cout << "Hello, world" << std::endl;
@@ -40,15 +67,6 @@ int main(int argc,char** argv)
 	//const char *const cmd[] = {"/usr/bin/ffmpeg", "-v","24","-pix_fmts", 0};
     const char* cmd[] = {"/usr/bin/ffmpeg", "-i", "test.mp4", "-pix_fmt", "rgba", "-vcodec", "rawvideo", "-f", "image2pipe", "pipe:1", 0};
 
-	manual_subprocess(cmd);
-
-    Subprocess sp{cmd};
-	/*if(sp.errstream.good())
-		std::cout << "errorstream good!" << std::endl;
-    std::cout << sp.errstream.rdbuf();
-	*/
-	if(sp.errstream.good())
-		std::cout << "outstream good!" << std::endl;
-    std::cout << sp.errstream.rdbuf();
+	manual_subprocess_class(cmd);
     return 0;
 }
