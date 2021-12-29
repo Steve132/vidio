7ef5773661781673de994f3b00dc592848844fe6
heretodev <heretodev@github.com>
2021-12-29 14:17:12 -0500

    Named pipe class works.  Test works.

--- named_pipe.cpp
+++ named_pipe.cpp
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <stdexcept>
 #include <iostream>
+#include <cerrno> // read error set by mkfifo with errno
+#include <cstring> // strerror for errno for mkfifo
 using namespace std;
 
 namespace named_pipe
@@ -14,24 +16,22 @@ class Reader::Impl
 public:
 	bool m_good;
 	std::string filename;
-    Impl(const std::string& filename_str)
-    {
+	Impl(const std::string& filename_str)
+	{
 		filename = filename_str;
 		// mkfifo(<pathname>,<permission>)
-		int ret = mkfifo(filename.c_str(), 0666);
-		if(ret < 0)
+		int ret = mkfifo(filename_str.c_str(), 0666);
+		if((ret < 0) && (errno != EEXIST))
 		{
 			m_good = false;
-			throw std::runtime_error("Could not open named pipe " + filename + ".");
+			throw std::runtime_error("Could not open named pipe " + filename + ".  " + std::strerror(errno));
 		}
 		m_good = true;
-    }
+	}
 	~Impl()
 	{
-		std::cerr << "destructor for reader->impl." << std::endl;
 		if(m_good)
 		{
-			std::cerr << "destructor for reader->impl calling unlink pipe: " << filename << std::endl;
 			unlink(filename.c_str());
 		}
 	}
@@ -46,26 +46,26 @@ public:
 			throw std::runtime_error("Could not open file for reading: " + filename + ".");
 			return false;
 		}
-        read(fd, tmpbuf, nbytes);
-        close(fd);
+		read(fd, tmpbuf, nbytes);
+		close(fd);
 
 		return true;
-    }
+	}
 };
 
 Reader::Reader(const std::string& filename):
-    impl(std::make_shared<Reader::Impl>(filename))
+	impl(std::make_shared<Reader::Impl>(filename))
 {}
 Reader::~Reader()
 {}
 
 bool Reader::good() const
 {
-    return impl && impl->good();
+	return impl && impl->good();
 }
 bool Reader::read_buf(void *buf, const std::size_t nbytes) const
 {
-    return impl->read_buf(buf, nbytes);
+	return impl->read_buf(buf, nbytes);
 }
 
 class Writer::Impl
@@ -73,61 +73,52 @@ class Writer::Impl
 public:
 	bool m_good;
 	std::string filename;
-    Impl(const std::string& filename_str)
-    {
+	Impl(const std::string& filename_str)
+	{
 		filename = filename_str;
 		// mkfifo(<pathname>,<permission>)
 		int ret = mkfifo(filename.c_str(), 0666);
 		if(ret < 0)
 		{
 			m_good = false;
-			throw std::runtime_error("Could not open named pipe " + filename + ".");
+			throw std::runtime_error("Could not open named pipe " + filename + ".  " + std::strerror(errno));
 		}
-		std::cerr << "mkfifo succeeded: " << filename << std::endl;
 		m_good = true;
-    }
+	}
 	~Impl()
 	{
-		std::cerr << "destructor for writer->impl." << std::endl;
 	}
 	bool good() const {return m_good;}
 
 	bool write_buf(void *buf, const std::size_t nbytes) const
 	{
-		std::cerr << "writing buf inside writer::impl:" << std::endl;
 		uint8_t* tmpbuf = static_cast<uint8_t*>(buf);
-		std::cerr << "cast to tmpbuf." << std::endl;
-		std::cerr << "opening file for writing:" << filename << std::endl;
 		// Note: opening the file and writing to it to act as lock for now.
 		int fd = open(filename.c_str(), O_WRONLY);
-		std::cerr << "opened file for writing:" << filename << " and fd:" << fd << std::endl;
 		if(fd < 0)
 		{
 			throw std::runtime_error("Could not open file for writing: " + filename + ".");
 			return false;
 		}
-		std::cerr << "attempting write tmpbuf:" << std::endl;
-		std::cerr.flush();
-        write(fd, tmpbuf, nbytes);
-		std::cerr << "wrote tmpbuf!" << std::endl;
-        close(fd);
+		write(fd, tmpbuf, nbytes);
+		close(fd);
 		return true;
-    }
+	}
 };
 
 Writer::Writer(const std::string& filename):
-    impl(std::make_shared<Writer::Impl>(filename))
+	impl(std::make_shared<Writer::Impl>(filename))
 {}
 Writer::~Writer()
 {}
 
 bool Writer::good() const
 {
-    return impl && impl->good();
+	return impl && impl->good();
 }
 bool Writer::write_buf(void *buf, const std::size_t nbytes) const
 {
-    return impl->write_buf(buf, nbytes);
+	return impl->write_buf(buf, nbytes);
 }
 
 }
--- test_named_pipe.cpp
+++ test_named_pipe.cpp
@@ -14,75 +14,18 @@ void test_writer(const std::string& filename, std::size_t bufsize = 8)
 	named_pipe::Writer writer(filename);
 	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
 
-	cout << "buf being written:" << endl;
-	uint8_t ch = 's';
-	for(size_t i = 0; i < bufsize; i++)
-	{
-		framebuf.get()[i] = ch;
-		cout << framebuf.get()[i] << endl;
-	}
-	cout.flush();
-
-	int iter = 10;
-	while (1)
-	{
-		cout << "writing iter: " << iter << endl;
-		writer.write_buf(framebuf.get(), bufsize);
-		sleep(1); // let the reader get the data.  TODO: O_NONBLOCK in writer/reader?  async?
-
-		if(iter < 0)
-		{
-			break;
-		}
-		iter --;
-	}
-}
-
-void test_reader(const std::string& filename, std::size_t bufsize = 8)
-{
-	named_pipe::Reader reader(filename);
-	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
-
-	std::size_t niters = 3;
-	for(size_t i = 0; reader.read_buf(framebuf.get(), bufsize) && i < niters;i++)
-	{
-		cout << "reading iter: " << i << endl;
-		for(int c = 0; c < bufsize; c++)
-		{
-			cout << (int)framebuf[c] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void test_writer_no_named_pipe_class(const std::string& filename, std::size_t bufsize = 8)
-{
-	int fd;
-
-	// FIFO file path
-	const char* myfifo = filename.c_str();
-
-	// Creating the named file(FIFO)
-	// mkfifo(<pathname>, <permission>)
-	mkfifo(myfifo, 0666);
-
 	// Initialize test buffer
-	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
 	uint8_t ch = 's';
 	for(size_t i = 0; i < bufsize; i++)
 	{
 		framebuf.get()[i] = ch;
-		cout << framebuf.get()[i] << endl;
 	}
 
 	uint8_t depth = 8;
 	while (1)
 	{
-		uint8_t* tmpbuf = static_cast<uint8_t*>(framebuf.get());
-		fd = open(myfifo, O_WRONLY);
-		tmpbuf[0] = depth;
-		write(fd, tmpbuf, bufsize);
-		close(fd);
+		framebuf.get()[0] = depth;
+		writer.write_buf(framebuf.get(), bufsize);
 		sleep(1);
 
 		if(depth == 0)
@@ -93,36 +36,28 @@ void test_writer_no_named_pipe_class(const std::string& filename, std::size_t bu
 	}
 }
 
-void test_reader_no_named_pipe_class(const std::string& filename, std::size_t bufsize = 8)
+void test_reader(const std::string& filename, std::size_t bufsize = 8)
 {
-	int fd1;
-
-	// FIFO file path
-	const char* myfifo = filename.c_str();
-
-	// Creating the named file(FIFO)
-	// mkfifo(<pathname>,<permission>)
-	mkfifo(myfifo, 0666);
-
+	named_pipe::Reader reader(filename);
 	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
 
 	while (1)
 	{
-		// First open in read only and read
-		fd1 = open(myfifo,O_RDONLY);
-		uint8_t* tmpbuf = static_cast<uint8_t*>(framebuf.get());
-		read(fd1, tmpbuf, bufsize);
-
-		// Print the read string and close
-		printf("User1: %d\n", tmpbuf[0]);
-		close(fd1);
-
-		if(tmpbuf[0] == 0)
+		try
+		{
+			reader.read_buf(framebuf.get(), bufsize);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "There was an exception! " << e.what() << endl;
+		}
+		//printf("User1: %d\n", framebuf.get()[0]);
+		cout << static_cast<int>(framebuf.get()[0]) << endl;
+		if(framebuf.get()[0] == 0)
 		{
 			break;
 		}
 	}
-	unlink(myfifo);
 }
 
 int main(int argc, char** argv)
@@ -141,13 +76,11 @@ int main(int argc, char** argv)
 	{
 		if(is_reader)
 		{
-			//test_reader(filename);
-			test_reader_no_named_pipe_class(filename);
+			test_reader(filename);
 		}
 		else
 		{
-			//test_writer(filename);
-			test_writer_no_named_pipe_class(filename);
+			test_writer(filename);
 		}
 	} 
 	catch(const std::exception& e)
