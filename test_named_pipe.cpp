d4cae1d0523df1595fa9036d2581caf5f8de4823
heretodev <heretodev@github.com>
2021-12-29 13:48:07 -0500

    Updated test for named pipe to use buffer.

--- test_named_pipe.cpp
+++ test_named_pipe.cpp
@@ -55,7 +55,7 @@ void test_reader(const std::string& filename, std::size_t bufsize = 8)
 	}
 }
 
-void test_writer_no_named_pipe_class(const std::string& filename)
+void test_writer_no_named_pipe_class(const std::string& filename, std::size_t bufsize = 8)
 {
 	int fd;
 
@@ -66,15 +66,26 @@ void test_writer_no_named_pipe_class(const std::string& filename)
 	// mkfifo(<pathname>, <permission>)
 	mkfifo(myfifo, 0666);
 
-	float depth = 8;
+	// Initialize test buffer
+	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
+	uint8_t ch = 's';
+	for(size_t i = 0; i < bufsize; i++)
+	{
+		framebuf.get()[i] = ch;
+		cout << framebuf.get()[i] << endl;
+	}
+
+	uint8_t depth = 8;
 	while (1)
 	{
+		uint8_t* tmpbuf = static_cast<uint8_t*>(framebuf.get());
 		fd = open(myfifo, O_WRONLY);
-		write(fd, &depth, sizeof(depth));
+		tmpbuf[0] = depth;
+		write(fd, tmpbuf, bufsize);
 		close(fd);
 		sleep(1);
 
-		if(depth < 0)
+		if(depth == 0)
 		{
 			break;
 		}
@@ -82,7 +93,7 @@ void test_writer_no_named_pipe_class(const std::string& filename)
 	}
 }
 
-void test_reader_no_named_pipe_class(const std::string& filename)
+void test_reader_no_named_pipe_class(const std::string& filename, std::size_t bufsize = 8)
 {
 	int fd1;
 
@@ -93,18 +104,20 @@ void test_reader_no_named_pipe_class(const std::string& filename)
 	// mkfifo(<pathname>,<permission>)
 	mkfifo(myfifo, 0666);
 
-	float depth;
+	std::unique_ptr<uint8_t[]> framebuf(new uint8_t[bufsize]);
+
 	while (1)
 	{
 		// First open in read only and read
 		fd1 = open(myfifo,O_RDONLY);
-		read(fd1, &depth, sizeof(depth));
+		uint8_t* tmpbuf = static_cast<uint8_t*>(framebuf.get());
+		read(fd1, tmpbuf, bufsize);
 
 		// Print the read string and close
-		printf("User1: %f\n", depth);
+		printf("User1: %d\n", tmpbuf[0]);
 		close(fd1);
 
-		if(depth < 0)
+		if(tmpbuf[0] == 0)
 		{
 			break;
 		}
