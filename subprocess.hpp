fe4db7382666b7ebb4b98dcf2705dc807e1a0b72
heretodev <heretodev@github.com>
2022-01-05 20:14:20 -0500

    Update CMakeLists.txt, audio_support_notes.txt, and subprocess.hpp

--- subprocess.hpp
+++ subprocess.hpp
@@ -3,6 +3,7 @@
 
 #include "subprocess.h"
 #include <stdexcept>
+#include <cstdio>
 
 struct Subprocess
 {
@@ -26,6 +27,11 @@ public:
         }
         this->input=subprocess_stdin(&process);
     }
+	unsigned int write_to_stdin(const char* buf,unsigned int n)
+	{
+		FILE* inp=subprocess_stdin(&process);
+		return (unsigned int)fwrite(buf, n,1,inp);
+	}
     
     unsigned int read_from_stdout(char* buf,unsigned int n)
     {
